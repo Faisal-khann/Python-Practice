8369b963e4a4d94e45a51535ee401ce0eeb92ee0
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-07-10 00:26:27 +0530

    git add -A

--- NumPy/npa_method.py
+++ NumPy/npa_method.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 # Join Np Array
 arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
@@ -8,26 +9,28 @@ print("After joining:", arr)
 print()
 
 # Joining Arrays Using Stack Functions
-#We pass a sequence of arrays that we want to join to the stack() method along with the axis
+# We pass a sequence of arrays that we want to join to the stack() method along with the axis
 arr = np.stack((arr1, arr2), axis=1)
-print("After Joining: ",arr)
+print("After Joining: ", arr)
 
 print()
 
 # Spliting Arrays ->Splitting is reverse operation of Joining.
-#We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
+# We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
 import numpy as np
+
 arr = np.array([1, 2, 3, 4, 5, 6])
 
 newarr = np.array_split(arr, 3)
 
-print("After spliting: ",newarr)
+print("After spliting: ", newarr)
 
 print()
 
 # Searching Arrays -> To search an array, use the where() method.
 
 import numpy as np
+
 # Q.Find the indexes where the value is 4:
 arr = np.array([1, 2, 3, 4, 5, 4, 4])
 x = np.where(arr == 4)
@@ -36,7 +39,8 @@ print(x)
 
 # Sorting Np Array
 import numpy as np
+
 arr = np.array([10, 30, 5, 20])
 print("After Sorting:", np.sort(arr))
 
-# 
+#
