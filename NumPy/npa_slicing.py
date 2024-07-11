0cd15f26a3ececcc011de0eec6a80f8d8d0612c1
Faisal-Khann <faisalkhanfaisal919@gmail.com>
2024-07-11 13:10:53 +0530

    git add -A

--- NumPy/npa_slicing.py
+++ NumPy/npa_slicing.py
@@ -21,3 +21,9 @@ print(arr[1:5:2])
 #Return every other element from the entire array:
 print(arr[::2])
 
+#Slicing 2-D Arrays
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+
+#From the second element, slice elements from index 1 to index 4 (not included):
+print(arr[1, 1:4]) # -> 0 for 1st arr and 1 for 2nd arr
