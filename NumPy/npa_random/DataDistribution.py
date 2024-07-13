bbc6b52ece9703fb786f166d7f5304c5c0c219b5
Faisal khan <faisalkhanfaisal496@gmail.com>
2024-07-14 01:30:46 +0530

    git add comment

--- NumPy/npa_random/DataDistribution.py
+++ NumPy/npa_random/DataDistribution.py
@@ -1,11 +1,12 @@
-#Data Distribution is a list of all possible
-# values, and how often each value occurs.
+'''Data Distribution is a list of all possible
+   values, and how often each value occurs.
 
-# Random Distribution -> A random distribution is a set of random numbers that follow a certain probability density function.
-# We can generate random numbers based on defined probabilities using the choice() method of the random module.
+   Random Distribution -> A random distribution is a set of random numbers that follow a certain probability density function.
+   We can generate random numbers based on defined probabilities using the choice() method of the random module.
 
-# Q. Generate a 1-D array containing 100 values, where each value has to be 3, 5, 7 or 9
-# and its probabilities is 0.1, 0.3, 0.6, 0.0
+Q. Generate a 1-D array containing 10 values, where each value has to be 3, 5, 7 or 9
+  and its probabilities is 0.1, 0.3, 0.6, 0.0
+'''
 
 from numpy import random
 x = random.choice([3, 5, 6, 9], p = [0.1, 0.3, 0.6, 0.0], size=(10))
