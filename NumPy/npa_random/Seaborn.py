12992af23ff869d4d2d87b5320e23ea32b8c8733
Faisal khan <faisalkhanfaisal496@gmail.com>
2024-07-15 19:57:48 +0530

    git add A

--- NumPy/npa_random/Seaborn.py
+++ NumPy/npa_random/Seaborn.py
@@ -1,5 +1,7 @@
-# Distplot stands for distribution plot, it takes as input an array and plots a curve corresponding to the distribution of points in the array.
-# Import the pyplot object of the Matplotlib module in your code using the following statement:
+'''
+Distplot stands for distribution plot, it takes as input an array and plots a curve corresponding to the distribution of points in the array.
+Import the pyplot object of the Matplotlib module in your code using the following statement:
+'''
 import matplotlib.pyplot as plt
 import seaborn as sns
 
