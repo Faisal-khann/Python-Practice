c74e5ef063ececc95bcb3f648d9249659abd8a69
Faisal khan <faisalkhanfaisal496@gmail.com>
2024-07-28 00:17:11 +0530

    git add -A

--- Recursion.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 0 or n ==1: # factorial 0 is 1 and fact 1 is 1 
-        return 1
-    else:
-        return n * factorial(n-1)
-    
-n = int(input("Enter a number"))
-print(f"Factorial of {n} is: {factorial(n)} ")
