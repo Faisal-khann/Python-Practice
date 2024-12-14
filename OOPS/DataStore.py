69832ca3f92ef48eebd8e8ec93113c28191fc50e
Faisal khan <faisalkhanfaisal496@gmail.com>
2024-12-14 08:21:37 +0530

    git add -A

--- OOPS/DataStore.py
+++ OOPS/DataStore.py
@@ -9,16 +9,18 @@ class Employee:
 
     # define method
     def display(self):
-        print(f"ID: {self.id}, NAME: {self.name}, SALARY: {self.salary}, COMPANY: {self.company}")
+        print(
+            f"ID: {self.id}, NAME: {self.name}, SALARY: {self.salary}, COMPANY: {self.company}"
+        )
         print()
 
 
 if __name__ == "__main__":
-        emp1 = Employee(101, "Tom", 45000)
-        emp2 = Employee(102, "Roy", 50000)
-        emp3 = Employee(103, "Alex", 42000)
+    emp1 = Employee(101, "Tom", 45000)
+    emp2 = Employee(102, "Roy", 50000)
+    emp3 = Employee(103, "Alex", 42000)
 
-        # Call the display method
-        emp1.display()
-        emp2.display()
-        emp3.display()
+    # Call the display method
+    emp1.display()
+    emp2.display()
+    emp3.display()
